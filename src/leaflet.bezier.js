63b9cc84997d2d09c93941dfaff5cb15afff6607
Quentin VAN EXTERGHEM <quentin.v@travelcar.com>
2019-08-14 17:18:45 +0200

    build package

--- src/leaflet.bezier.js
+++ src/leaflet.bezier.js
@@ -1,21 +1,20 @@
+'use strict';
+
 L.SVG.include({
-    _updatecurve: function (layer) {
-        let svg_path = this._curvePointsToPath(layer._points);
+    _updatecurve: function _updatecurve(layer) {
+        var svg_path = this._curvePointsToPath(layer._points);
         this._setPath(layer, svg_path);
 
         if (layer.options.animate) {
-            let path = layer._path;
-            let length = path.getTotalLength();
+            var path = layer._path;
+            var length = path.getTotalLength();
 
             if (!layer.options.dashArray) {
                 path.style.strokeDasharray = length + ' ' + length;
             }
 
             if (layer._initialUpdate) {
-                path.animate([
-                    {strokeDashoffset: length},
-                    {strokeDashoffset: 0}
-                ], layer.options.animate);
+                path.animate([{ strokeDashoffset: length }, { strokeDashoffset: 0 }], layer.options.animate);
                 layer._initialUpdate = false;
             }
         }
@@ -23,49 +22,43 @@ L.SVG.include({
         return svg_path;
     },
 
-
-    _curvePointsToPath: function (points) {
-        let point, curCommand, str = '';
-        for (let i = 0; i < points.length; i++) {
+    _curvePointsToPath: function _curvePointsToPath(points) {
+        var point = void 0,
+            curCommand = void 0,
+            str = '';
+        for (var i = 0; i < points.length; i++) {
             point = points[i];
             if (typeof point === 'string' || point instanceof String) {
                 curCommand = point;
                 str += curCommand;
-            } else
-                str += point.x + ',' + point.y + ' ';
-
-
+            } else str += point.x + ',' + point.y + ' ';
         }
         return str || 'M0 0';
-    },
+    }
 
 });
 
-let Bezier = L.Path.extend({
+var Bezier = L.Path.extend({
     options: {},
-    initialize: function (path, icon, options) {
+    initialize: function initialize(path, icon, options) {
 
         if (!path.mid || path.mid[0] === undefined) {
-            path.mid = this.getMidPoint(path.from, path.to, (path.from.deep ? path.from.deep : 4), path.from.slide);
+            path.mid = this.getMidPoint(path.from, path.to, path.from.deep ? path.from.deep : 4, path.from.slide);
         }
 
         L.setOptions(this, options);
         this._initialUpdate = true;
         this.setPath(path);
         this.icon = icon;
-
     },
     //Juast after path is added
-    onAdd: function (map) {
+    onAdd: function onAdd(map) {
         this._renderer._initPath(this);
         this._reset();
         this._renderer._addPath(this);
 
         // TODO ajust plane acording to zoom
-        map.on('zoom', function () {
-
-        });
-
+        map.on('zoom', function () {});
     },
     setAnimatePlane: function setAnimatePlane(path) {
         if (this.spaceship_img) this.spaceship_img.remove();
@@ -79,7 +72,7 @@ let Bezier = L.Path.extend({
         width = Math.min(Math.max(width, 30), 64);
         height = Math.min(Math.max(height, 30), 64);
         var last_step = 0;
-        var anim = function(){
+        var anim = function anim() {
             Snap.animate(0, full_path_length, function (step) {
                 spaceship_img.attr({ visibility: "visible" });
                 spaceship_img.attr({ width: width, height: height });
@@ -90,88 +83,89 @@ let Bezier = L.Path.extend({
                 spaceship.transform('translate(' + x + ',' + y + ') rotate(' + (moveToPoint.alpha - 90) + ', ' + width / 2 + ', ' + height / 2 + ')');
             }, 5000, mina.easeinout, function () {
                 //done - repeat
-                anim()
+                anim();
             });
         };
         anim();
     },
-    getPath: function () {
+    getPath: function getPath() {
         return this._coords;
     },
-    setPath: function (path) {
+    setPath: function setPath(path) {
         this._setPath(path);
         return this.redraw();
     },
-    getBounds: function () {
+    getBounds: function getBounds() {
         return this._bounds;
     },
-    getMidPoint: function (from, to, deep, round_side = 'LEFT_ROUND') {
+    getMidPoint: function getMidPoint(from, to, deep) {
+        var round_side = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'LEFT_ROUND';
 
-        let offset = 3.14;
 
-        if (round_side === 'RIGHT_ROUND')
-            offset = offset * -1;
+        var offset = 3.14;
 
-        let latlngs = [];
+        if (round_side === 'RIGHT_ROUND') offset = offset * -1;
 
-        let latlng1 = from,
+        var latlngs = [];
+
+        var latlng1 = from,
             latlng2 = to;
 
-        let offsetX = latlng2.lng - latlng1.lng,
+        var offsetX = latlng2.lng - latlng1.lng,
             offsetY = latlng2.lat - latlng1.lat;
 
-        let r = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)),
+        var r = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)),
             theta = Math.atan2(offsetY, offsetX);
 
-        let thetaOffset = (offset / (deep ? deep : 4));
+        var thetaOffset = offset / (deep ? deep : 4);
 
-        let r2 = (r / 2) / (Math.cos(thetaOffset)),
+        var r2 = r / 2 / Math.cos(thetaOffset),
             theta2 = theta + thetaOffset;
 
-        let midpointX = (r2 * Math.cos(theta2)) + latlng1.lng,
-            midpointY = (r2 * Math.sin(theta2)) + latlng1.lat;
+        var midpointX = r2 * Math.cos(theta2) + latlng1.lng,
+            midpointY = r2 * Math.sin(theta2) + latlng1.lat;
 
-        let midpointLatLng = [midpointY, midpointX];
+        var midpointLatLng = [midpointY, midpointX];
 
         latlngs.push(latlng1, midpointLatLng, latlng2);
 
         return midpointLatLng;
     },
-    _setPath: function (path) {
+    _setPath: function _setPath(path) {
         this._coords = path;
         this._bounds = this._computeBounds();
     },
-    _computeBounds: function () {
+    _computeBounds: function _computeBounds() {
 
-        let bound = new L.LatLngBounds();
+        var bound = new L.LatLngBounds();
 
         bound.extend(this._coords.from);
-        bound.extend(this._coords.to);//for single destination
+        bound.extend(this._coords.to); //for single destination
         bound.extend(this._coords.mid);
 
         return bound;
     },
-    getCenter: function () {
+    getCenter: function getCenter() {
         return this._bounds.getCenter();
     },
-    _update: function () {
+    _update: function _update() {
         if (!this._map) {
             return;
         }
         this._updatePath();
     },
-    _updatePath: function () {
+    _updatePath: function _updatePath() {
         //animated plane
-        let path = this._renderer._updatecurve(this);
+        var path = this._renderer._updatecurve(this);
         this.setAnimatePlane(path);
     },
-    _project: function () {
+    _project: function _project() {
 
         this._points = [];
 
         this._points.push('M');
 
-        let curPoint = this._map.latLngToLayerPoint(this._coords.from);
+        var curPoint = this._map.latLngToLayerPoint(this._coords.from);
         this._points.push(curPoint);
 
         if (this._coords.mid) {
@@ -181,22 +175,19 @@ let Bezier = L.Path.extend({
         }
         curPoint = this._map.latLngToLayerPoint(this._coords.to);
         this._points.push(curPoint);
-
-
-    },
-
+    }
 
 });
 
 L.bezier = function (config, options) {
-    let paths = [];
-    for (let i = 0; config.path.length > i; i++) {
-        let last_destination = false;
-        for (let c = 0; config.path[i].length > c; c++) {
+    var paths = [];
+    for (var i = 0; config.path.length > i; i++) {
+        var last_destination = false;
+        for (var c = 0; config.path[i].length > c; c++) {
 
-            let current_destination = config.path[i][c];
+            var current_destination = config.path[i][c];
             if (last_destination) {
-                let path_pair = {from: last_destination, to: current_destination};
+                var path_pair = { from: last_destination, to: current_destination };
                 paths.push(new Bezier(path_pair, config.icon, options));
             }
 
@@ -204,7 +195,4 @@ L.bezier = function (config, options) {
         }
     }
     return L.layerGroup(paths);
-
 };
-
-
